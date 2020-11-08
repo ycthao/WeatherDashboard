719a3cf4723ee9887c97a19cc27f465839f2a521
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-08 17:42:59 -0600

    put on click event to search icon and remove hide class

---
+++ assets/js/weather.js
@@ -0,0 +1,10 @@
+
+// when click search icon do this...
+$("#search").on("click", function() {
+
+    // remove hide class
+    $("#searchHistory").removeClass("hide");
+    $("#weather").removeClass("hide");
+
+});
+

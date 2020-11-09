821b8ff09017dd9dd3543b2535dbfacd9f523c12
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-08 18:02:08 -0600

    adding ajax call, and setting varieable

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,3 +1,16 @@
+let key = "972b67e55da3225a8c828abcb14c0c59";
+let city = "milwaukee";
+let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ city + "&appid=" + key ;
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+
+    console.log(response);
+  //$("#city").text(JSON.stringify(response));
+});
+
 
 // when click search icon do this...
 $("#search").on("click", function() {
